6dbd32c07e0cd22df3bb239a22ca5a81fdc5ccf9
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-05-11 12:18:32 +0800

    2023-05-11T12-18-32Z

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ model_slug_to_model_name_map = {
     'text-davinci-002-render-paid': 'gpt-3.5-turbo',
     'text-davinci-002-render': 'gpt-3.5-turbo',
     'gpt-4': 'gpt-4',
-    'text-davinci-002-plugins': 'plugins'
+    'text-davinci-002-plugins': 'plugins',
 }
 
 
@@ -92,3 +92,43 @@ def search_and_extract_preview(
         return message[start_index:end_index]
     else:
         return ''
+
+
+import os
+import platform
+from pathlib import Path
+
+
+def get_creation_time(file_path: os.PathLike) -> float | int:
+    if platform.system() == "Windows":
+        return os.path.getctime(file_path)
+    else:
+        stat = os.stat(file_path)
+        try:
+            return stat.st_birthtime
+        except AttributeError:
+            return stat.st_ctime
+
+
+def find_last_added_file(
+    dir_path: os.PathLike, glob_pattern: str | None = None, recursive: bool = False
+) -> Path:
+    """
+    Find the last added file in a directory.
+
+    :param dir_path: Path to the directory to search in.
+    :param glob_pattern: Glob pattern to match files.
+    :param recursive: Search recursively.
+    :return: Path to the last added file.
+    """
+    dir_path = Path(dir_path)
+    if glob_pattern is None:
+        files = dir_path.iterdir()
+    else:
+        if recursive:
+            files = dir_path.rglob(glob_pattern)
+        else:
+            files = dir_path.glob(glob_pattern)
+
+    last_added_file = max(files, key=get_creation_time)
+    return last_added_file
